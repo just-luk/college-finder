cd23a3478ed5ad5dd8d292a9bd8cac41b8a351a3
Luke Griffith <lukegriffith@gmail.com>
2023-04-19 18:38:55 +0000

    change colors

--- apps/expo/src/screens/signin.tsx
+++ apps/expo/src/screens/signin.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-import { View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 
 import SignInWithOAuth from "../components/SignInWithOAuth";
 
 export const SignInSignUpScreen = () => {
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <View className="h-full w-full p-4">
+    <SafeAreaView className="bg-black">
+      <View className="h-full w-full p-16">
+        <Text className="text-7xl text-center text-white mb-10">Sign In</Text>
         <SignInWithOAuth />
       </View>
     </SafeAreaView>
